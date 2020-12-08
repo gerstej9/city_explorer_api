105219456373e8986bab80d6f1cb1460b5bd8c62
James Gerstenberger <gerstej9@gmail.com>
2020-12-07 17:22:53 -0800

    removing zombie code part 2

--- server.js
+++ server.js
@@ -3,19 +3,14 @@
 
 
 const express = require('express');
-const cors = require('cors'); // Cross origin resource sharing - allow a frontend running from your computer to access a backend on your computer
-require('dotenv').config(); // configures variables from `.env`
-// npm install dotenv -S
-// npm i dotenv -S
+const cors = require('cors');
+require('dotenv').config();
+
 
 const app = express();
 const PORT = process.env.PORT || 3002;
 
 app.use(cors());
-
-// app.get("/home", function(req, res){
-//     res.send("./index.html");
-// });
 
 app.get('/location', function(req, res){
     const locationData = require('./data/location.json');
